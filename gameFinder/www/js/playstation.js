d16e6e938ad9aee36460636c9feba5c4506482c6
jannik jungmann <jungmannjannik@gmail.com>
2025-02-12 19:24:54 +0100

    Added rawg api

--- gameFinder/www/js/playstation.js
+++ gameFinder/www/js/playstation.js
@@ -1,6 +1,6 @@
 // Replace with your actual RAWG API key and base URL.
 const API_KEY = "5b222bbaa5a744f6a1b9646bf3d1af3a";
-const platform = "1"; // For example, Xbox One (verify with RAWG docs)
+const platform = "187"; // For example, Xbox One (verify with RAWG docs)
 const RAWG_BASE_URL = "https://api.rawg.io/api";
 
 // Local cache for games
